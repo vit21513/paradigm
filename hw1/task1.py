fd52ef3fcac84bce0a694d1f4a4bb20900360602
vit21513 <vit21513@yandex.ru>
2023-11-11 11:26:58 +0300

    first commit

---
+++ hw1/task1.py
@@ -0,0 +1,17 @@
+def sort_list_impertive(numbers):
+    for i in range(0, len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            if numbers[i] <= numbers[j]:
+                numbers[i], numbers[j] = numbers[j], numbers[i]
+    return numbers
+
+
+def sort_list_declarative(numbers: list):
+    numbers.sort(reverse=True)
+    return numbers
+
+
+if __name__ == "__main__":
+    num = [5, 6, 8, 23, 4, 1, 0, 3]
+    print(sort_list_impertive(num))
+    print(sort_list_declarative(num))
